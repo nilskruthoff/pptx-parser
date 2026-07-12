bee7ce9f1397829f702feefcb35903af74f0a068
nilsk <nils.kruthoff@iubh-fernstudium.de>
2026-07-12 16:11:28 +0200

    adding constants for the namespaces of slide master and layout

--- src/constants.rs
+++ src/constants.rs
@@ -2,3 +2,5 @@
 pub const A_NAMESPACE: &str = "http://schemas.openxmlformats.org/drawingml/2006/main";
 pub const RELS_NAMESPACE: &str = "http://schemas.openxmlformats.org/officeDocument/2006/relationships";
 pub const IMAGE_NAMESPACE: &str = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/image";
+pub const SLIDE_LAYOUT_NAMESPACE: &str = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideLayout";
+pub const SLIDE_MASTER_NAMESPACE: &str = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideMaster";
